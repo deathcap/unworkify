854f618c63fa254fef9810963d816cff0a296216
deathcap <deathcap@gmx.co.uk>
2014-01-21 20:48:43 -0800

    Augment instance instead of prototype

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ module.exports = function(fn) {
 
   var self = new fn();
 
-  fn.prototype.addEventListener = function(ev, cb) {
+  self.addEventListener = function(ev, cb) {
     self.on(ev,cb);
-    //console.log('addEventListener',ev,cb);
   };
 
   global.postMessage = // unfortunately global for worker (no namespaces?)
-  fn.prototype.postMessage = function(msg) {
+  self.postMessage = function(msg) {
     self.emit('message', {data:msg});
-    //console.log('postMessage',msg);
   };
 
   return self;
