23933e6cfc4b326642cb70fcd1ad2f11f56eaaaf
deathcap <deathcap@gmx.co.uk>
2014-01-21 20:57:18 -0800

    Use unworkify in node, webworkify in browser

---
+++ example/README.md
@@ -0,0 +1,6 @@
+Based on example from [https://github.com/substack/webworkify](https://github.com/substack/webworkify)
+
+To use in browser (with webworkify):
+
+    `beefy main.js`
+
--- example/main.js
+++ example/main.js
@@ -1,4 +1,12 @@
-var work = require('../');
+var work;
+
+if (process.browser) {
+  work = require('webworkify');
+  console.log('Using webworkify');
+} else {
+  work = require('../');
+  console.log('Using unworkify');
+}
 
 var w = work(require('./worker.js'));
 w.addEventListener('message', function (ev) {
