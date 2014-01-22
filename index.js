669494f3d10ccbdb542eebc865ecd6c6d56c2eb8
deathcap <deathcap@gmx.co.uk>
2014-01-21 20:34:14 -0800

    inherits

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('inherits');
+
 module.exports = function(fn) {
 
+  inherits(fn, EventEmitter);
+  console.log('fN',fn);
+
   fn.addEventListener = function(ev, cb) {
+    this.on(ev, cb);
     console.log('addEventListener',ev,cb);
   };
 
